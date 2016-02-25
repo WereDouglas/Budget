fdc3e626b0faa42acdafa1b8a2e73dbd1e1452e8
WereDouglas <weredouglas@gmail.com>
2016-02-25 18:20:16 +0300

    Added api

--- application/controllers/Budget.php
+++ application/controllers/Budget.php
@@ -67,7 +67,6 @@ class Budget extends CI_Controller {
 
         $this->load->view('add-budget', $data);
     }
-
     public function import() {
 
         if (isset($_POST["Import"])) {
@@ -200,6 +199,91 @@ class Budget extends CI_Controller {
         
         echo '<div class="alert alert-info">   <strong>Information uploaded!  </strong>	</div>';
         // $this->load->view('add-budget', $data);
+
+    }
+
+
+    public function sync() {
+
+        
+            
+                        $budget = new stdClass();
+                        $budget->period = $_POST["period"];
+                         $budget->department = $_POST["department"];
+                        $budget->unit = $_POST["unit"];
+                        $budget->activity = $_POST["activity"];
+                        $budget->output = $_POST["output"];
+                        $budget->outcome = $_POST["outcome"];
+                        $budget->objective = $_POST["objective"];
+                        $budget->initiative = $_POST["initiative"];
+                        $budget->performance = $_POST["performance"];
+                        $budget->starts = $_POST["starts"];
+                       
+                        $budget->Procurement_type = $_POST["procurement_type"];
+                        $budget->category = $_POST["category"];
+                        $budget->line = $_POST["line"];
+                        $budget->subline = $_POST["subline"];
+                        $budget->account = $_POST["account"];
+                        $budget->funding = $_POST["funding"];
+                        $budget->account_description = $_POST["account_description"];
+                        
+                        $budget->unit = $_POST["unit"];
+                        $budget->currency = $_POST["currency"] ;
+						$budget->rate = $_POST["rate"];
+                        $budget->price = $_POST["price"];
+                        $budget->qty = $_POST["qty"];
+                        $budget->persons = $_POST["persone"];
+                        $budget->freq = $_POST["freq"];
+                        $budget->priceL = $_POST["priceL"];
+                        $budget->total = $_POST["total"];
+                    
+                        $budget->flow = $_POST["flow"];
+                        $budget->totalL = $_POST["totalL"];
+                        $budget->radio = " ";
+                        $budget->variance = $_POST["variance"];
+                     
+                        $budget->generation = $_POST["generation"];
+                        $budget->January = $_POST["January"];
+                        $budget->February = $_POST["February"];
+                        $budget->March = $_POST["March"];
+                        $budget->April = $_POST["April"];
+                        $budget->May = $_POST["May"];
+                        $budget->June = $_POST["June"];
+                        $budget->July = $_POST["July"];
+                        $budget->August = $_POST["August"];
+                        $budget->September = $_POST["September"];
+                        $budget->October = $_POST["October"];
+                        $budget->November = $_POST["November"];
+                        $budget->December = $_POST["December"];
+                        $budget->Quarter1 = $_POST["Quarter1"];
+                        $budget->Quarter2 = $_POST["Quarter2"];
+                        $budget->Quarter3 = $_POST["Quarter3"];
+                        $budget->Quarter4 = $_POST["Quarter4"];
+                        $budget->services = $_POST["services"];
+                       
+                        $budget->details = $_POST["details"];
+                        $budget->Year = $_POST["Year"];
+
+
+                        $budget = json_encode($budget);
+
+                       
+                        
+                   
+       if ($_POST["period"] == "") {
+            echo 'F';
+            return;
+        }
+        //echo $date;      
+//        $get_result = $this->Md->check($datetime, 'datetime', 'metar');
+//        if (!$get_result) {
+//                 echo 'T' ;
+//        }        
+        else {
+             $instance = array('account' => $_POST["account"], 'total' => $_POST["total"], 'enddate' => "", 'startdate' => "", 'initiative' => $_POST["initiative"], 'unit' => $_POST["unit"], 'department' => str_replace("_"," ",$_POST["department"]), 'period' => $_POST["period"], 'orgID' => '', 'content' => $budget, 'by' => $this->session->userdata('email'), 'created' => date('Y-m-d H:i:s'));
+             $id = $this->Md->save($instance, 'instance');
+            echo 'T';
+        }
 
     }
 
